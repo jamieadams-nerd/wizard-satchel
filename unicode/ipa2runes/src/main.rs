7f12131c1ad5b417f2423a8718dea3b90f382637
Jamie Adams <jamie_l_adams@icloud.com>
2026-02-01 15:01:14 -0500

    Update main.rs
    Added MIT copyright flag

--- unicode/ipa2runes/src/main.rs
+++ unicode/ipa2runes/src/main.rs
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2026 Jamie Adams (a.k.a, Imodium Operator)
+//
 /*!
 ===============================================================================
 ipa2runes — IPA → Runic Transliteration Tool
